49a96e6ceafcbd60f05fc6d5082b6c3a9f0c8bfb
Blandeu <blandregion@gmail.com>
2023-04-20 10:33:09 +0300

    melondrop v2

--- watermelon_game/watermelon.js
+++ watermelon_game/watermelon.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 
 let x = 100;
 let y = 100;
-let radius = 50;
+let radius = 20;
 let speed = 10;
 
 let upPressed = false;
@@ -12,11 +12,30 @@ let leftPressed = false;
 let rightPressed = false;
 
 function drawGame(){
-    console.log("draw");
     requestAnimationFrame(drawGame);
     clearScreen();
     inputs();
+    boundryCheck();
     drawPurBlob();
+}
+
+function boundryCheck(){
+    //UP
+    if( y < radius){
+        y = radius;
+    }
+    //DOWN
+    if(y > canvas.height - radius) {
+        y = canvas.height - radius;
+    }
+    //LEFT
+    if(x < radius) {
+        x = radius;
+    }
+    //RIGHT
+    if(x > canvas.width - radius) {
+        x = canvas.width - radius;
+    }
 }
 
 function inputs(){
@@ -34,15 +53,27 @@ function inputs(){
     }
 }
 
-function drawPurBlob(){
+function drawPurBlob() {
     ctx.fillStyle = "pink";
+if(upPressed) {
+    ctx.fillStyle = "purple";
+}
+if(downPressed) {
+    ctx.fillStyle = "red";
+}
+if(leftPressed) {
+    ctx.fillStyle = "violet";
+}
+if(rightPressed) {
+    ctx.fillStyle = "magenta";
+}
     ctx.beginPath();
-    ctx.arc(x,y, radius,0, Math.PI * 1);
+    ctx.arc(x,y, radius,0, Math.PI * 2);
     ctx.fill();
 }
 
 function clearScreen(){
-    ctx.fillStyle = "transparent";
+    ctx.fillStyle = "blue";
     ctx.fillRect(0,0, canvas.width, canvas.height);
 }
 
@@ -55,44 +86,44 @@ document.body.addEventListener('keyup', keyUp);
 
 function keyDown(event) {
     //up
-    if(event.keyCode == 38) {
+    if(event.keyCode == 87) {
             upPressed = true;
     }
 
     //down
-    if(event.keyCode == 40){
+    if(event.keyCode == 83){
         downPressed = true;
     }
 
     //left
-    if(event.keyCode == 37){
+    if(event.keyCode == 65){
         leftPressed = true;
     }
 
     //right
-    if(event.keyCode == 39){
+    if(event.keyCode == 68){
         rightPressed = true;
     }
 }
 
 function keyUp(event) {
     //up
-    if(event.keyCode == 38) {
+    if(event.keyCode == 87) {
         upPressed = false;
 }
 
     //down
-    if(event.keyCode == 40){
+    if(event.keyCode == 83){
         downPressed = false;
 }
 
     //left
-    if(event.keyCode == 37){
+    if(event.keyCode == 65){
         leftPressed = false;
 }
 
     //right
-    if(event.keyCode == 39){
+    if(event.keyCode == 68){
         rightPressed = false;
 }
 }
