714d457ba1de416cdd654c71522d578027e76245
Blandeu <blandregion@gmail.com>
2023-04-27 09:06:57 +0300

    Loop exercises and a number guessinggame

--- 17.2/scripts/welcome.js
+++ 17.2/scripts/welcome.js
@@ -1,6 +1,8 @@
-var name = prompt("What is your name?")
-var welcomeNode = document.getElementById("welcome")
-
-if(name.toLowerCase() == "benkku") {
-    welcomeNode.innerText = "Welcome to your website";
-}
+ var name = prompt("What is your name?")
+  var welcomeNode = document.getElementById("welcome")
+  
+  if(name.toLowerCase() == "benkku") {
+      welcomeNode.innerText = "Welcome to your website";
+  } else {
+      welcomeNode.innerText = "Welcome to Benkku's Website"
+  }
