d8f94aecce4d5fd8a5c1c5fb91553f8e369ab16f
Blandeu <blandregion@gmail.com>
2023-05-26 10:45:53 +0300

    Changes within the final project

--- final_project/final.js
+++ final_project/final.js
@@ -1,18 +1,26 @@
+// SPLITTING the Characters \\
+
 const h1 = document.querySelector("h1")
-const letters = h1.innerText.split('')
+const characters = h1.innerText.split('')
 let html = ""
 
-letters.forEach(letter => {
+// SELECTING the Characters \\
+
+characters.forEach(letter => {
     let classes = ""
     if(letter !== " ") {
-        classes = "letter js-letter"
+        classes = "letter javascript-character"
     }
     html = html + `<span class='${classes}'>${letter}</span>`
 })
 
 h1.innerHTML = html
-const jsLetters = document.querySelectorAll(".js-letter")
-jsLetters.forEach(node => {
+
+const jsCharacters = document.querySelectorAll(".javascript-character")
+jsCharacters.forEach(node => {
+
+    // mouseover/mouseout function, hovering over and off the text Event Listeners \\
+
     node.addEventListener("mouseover", function(event) {
         console.log(this.innerText)
         this.classList.add("active")
