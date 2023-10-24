d4489df72e6151f22c394ff76bf32f47210ce4c1
Benjamin Wallenius <benjamin.wallenius.22@edu.luksia.fi>
2023-10-24 13:49:04 +0300

    html/bootstrap/javascript exercises

---
+++ ohjelmistok/tapahtumankäsittelijä/kertausjs3.js
@@ -0,0 +1,6 @@
+const element = document.getElementById("painike");
+element.addEventListener("click", myFunction);
+
+function myFunction() {
+document.getElementById("viikonpaiva").innerHTML = "Tänään on tiistai";
+}
