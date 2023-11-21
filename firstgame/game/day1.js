5891785ae39a632bb3d8fc6fa54f8df9915b848a
Benjamin Wallenius <benjamin.wallenius.22@edu.luksia.fi>
2023-11-21 09:11:00 +0200

    php exercises and kaboom js game start

---
+++ firstgame/game/day1.js
@@ -0,0 +1,33 @@
+//kaboom(
+//    {   width: 360, 
+//        height: 740, 
+//        canvas: document.getElementById("game_canvas"),
+//        background: [ 0, 0, 255, ],
+//    });
+
+const map = [
+    '                           ',
+    '                           ',
+    '                           ',
+    '                           ',
+    '          $                ',
+    '    %   =^=%=              ',
+    '                           ',
+    '                       -+  ',
+    '        $    ^   ^  ()     ',
+    '======================   ==',
+]
+
+const levelCfg = {
+    width: 20,
+    height: 20,
+    '=' :  [sprite('2')],
+    '$' :  [sprite('Mushroom_2')],
+    '%' :  [sprite('Mushroom_1'), 'mushroom_1-surprise'],
+    '(' :  [sprite('1')],
+    ')' :  [sprite('3')],
+    '-' :  [sprite('13')],
+    '+' :  [sprite('15')]
+}
+
+addLevel(map, levelCfg)
